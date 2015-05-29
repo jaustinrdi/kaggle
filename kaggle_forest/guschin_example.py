26ed99e80fce3945642d85836483ab8fc42027ec
jaustinrdi <jaustin@resdat.com>
2015-05-29 15:01:10 -0800

    more performant method (0.80965)

--- kaggle_forest/guschin_example.py
+++ kaggle_forest/guschin_example.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from sklearn import ensemble
-#%matplotlib inline
+# %matplotlib inline
 import matplotlib.pyplot as plt
 import numpy as np
 from IPython.display import Image
+import time
+from datetime import datetime
 
 # Competition: https://www.kaggle.com/c/forest-cover-type-prediction/
 
@@ -11,25 +13,29 @@ from IPython.display import Image
 # http://nbviewer.ipython.org/github/aguschin/kaggle/blob/master/forestCoverType_featuresEngineering.ipynb
 # and modified somewhat
 
+print("%s" % time.time() + " : " + "Start")
+
+print("%s" % time.time() + " : " + "Loading data...")
 
 train = pd.read_csv('files/train.csv')
 test = pd.read_csv('files/test.csv')
 
 # Inspect data
 
-train.ix[:0,:11]
+train.ix[:0, :11]
 
-train.ix[:,:11].hist(figsize=(16,12),bins=50)
-#plt.show()
+train.ix[:, :11].hist(figsize=(16, 12), bins=50)
+# plt.show()
 
 
 # As we know about that Aspect is in degrees azimuth, we can try to shift it at 180
 
 def r(x):
-    if x+180>360:
-        return x-180
+    if x + 180 > 360:
+        return x - 180
     else:
-        return x+180
+        return x + 180
+
 
 train['Aspect2'] = train.Aspect.map(r)
 test['Aspect2'] = test.Aspect.map(r)
@@ -43,9 +49,8 @@ test['Highwater'] = test.Vertical_Distance_To_Hydrology < 0
 # Take a look at Elevation and Vertical_Distance_To_Hydrology. Color each cover type in unique color to see some patterns
 
 
-def plotc(c1,c2):
-
-    fig = plt.figure(figsize=(16,8))
+def plotc(c1, c2):
+    fig = plt.figure(figsize=(16, 8))
     sel = np.array(list(train.Cover_Type.values))
 
     plt.scatter(c1, c2, c=sel, s=100)
@@ -59,39 +64,41 @@ def plotc(c1,c2):
 # plotc(train.Elevation-train.Vertical_Distance_To_Hydrology, train.Vertical_Distance_To_Hydrology)
 
 # Fine! Now we can add this new feature to train and test (the same goes for Horizontal_Distance_To_Hydrology, you can check this by youself)
-train['EVDtH'] = train.Elevation-train.Vertical_Distance_To_Hydrology
-test['EVDtH'] = test.Elevation-test.Vertical_Distance_To_Hydrology
+train['EVDtH'] = train.Elevation - train.Vertical_Distance_To_Hydrology
+test['EVDtH'] = test.Elevation - test.Vertical_Distance_To_Hydrology
 
-train['EHDtH'] = train.Elevation-train.Horizontal_Distance_To_Hydrology*0.2
-test['EHDtH'] = test.Elevation-test.Horizontal_Distance_To_Hydrology*0.2
+train['EHDtH'] = train.Elevation - train.Horizontal_Distance_To_Hydrology * 0.2
+test['EHDtH'] = test.Elevation - test.Horizontal_Distance_To_Hydrology * 0.2
 
 
 # Finally, we can create some features from those which have similar meanings, f.e. distances
-train['Distanse_to_Hydrolody'] = (train['Horizontal_Distance_To_Hydrology']**2+train['Vertical_Distance_To_Hydrology']**2)**0.5
-test['Distanse_to_Hydrolody'] = (test['Horizontal_Distance_To_Hydrology']**2+test['Vertical_Distance_To_Hydrology']**2)**0.5
+train['Distanse_to_Hydrolody'] = (train['Horizontal_Distance_To_Hydrology'] ** 2 + train[
+    'Vertical_Distance_To_Hydrology'] ** 2) ** 0.5
+test['Distanse_to_Hydrolody'] = (test['Horizontal_Distance_To_Hydrology'] ** 2 + test[
+    'Vertical_Distance_To_Hydrology'] ** 2) ** 0.5
 
-train['Hydro_Fire_1'] = train['Horizontal_Distance_To_Hydrology']+train['Horizontal_Distance_To_Fire_Points']
-test['Hydro_Fire_1'] = test['Horizontal_Distance_To_Hydrology']+test['Horizontal_Distance_To_Fire_Points']
+train['Hydro_Fire_1'] = train['Horizontal_Distance_To_Hydrology'] + train['Horizontal_Distance_To_Fire_Points']
+test['Hydro_Fire_1'] = test['Horizontal_Distance_To_Hydrology'] + test['Horizontal_Distance_To_Fire_Points']
 
-train['Hydro_Fire_2'] = abs(train['Horizontal_Distance_To_Hydrology']-train['Horizontal_Distance_To_Fire_Points'])
-test['Hydro_Fire_2'] = abs(test['Horizontal_Distance_To_Hydrology']-test['Horizontal_Distance_To_Fire_Points'])
+train['Hydro_Fire_2'] = abs(train['Horizontal_Distance_To_Hydrology'] - train['Horizontal_Distance_To_Fire_Points'])
+test['Hydro_Fire_2'] = abs(test['Horizontal_Distance_To_Hydrology'] - test['Horizontal_Distance_To_Fire_Points'])
 
-train['Hydro_Road_1'] = abs(train['Horizontal_Distance_To_Hydrology']+train['Horizontal_Distance_To_Roadways'])
-test['Hydro_Road_1'] = abs(test['Horizontal_Distance_To_Hydrology']+test['Horizontal_Distance_To_Roadways'])
+train['Hydro_Road_1'] = abs(train['Horizontal_Distance_To_Hydrology'] + train['Horizontal_Distance_To_Roadways'])
+test['Hydro_Road_1'] = abs(test['Horizontal_Distance_To_Hydrology'] + test['Horizontal_Distance_To_Roadways'])
 
-train['Hydro_Road_2'] = abs(train['Horizontal_Distance_To_Hydrology']-train['Horizontal_Distance_To_Roadways'])
-test['Hydro_Road_2'] = abs(test['Horizontal_Distance_To_Hydrology']-test['Horizontal_Distance_To_Roadways'])
+train['Hydro_Road_2'] = abs(train['Horizontal_Distance_To_Hydrology'] - train['Horizontal_Distance_To_Roadways'])
+test['Hydro_Road_2'] = abs(test['Horizontal_Distance_To_Hydrology'] - test['Horizontal_Distance_To_Roadways'])
 
-train['Fire_Road_1'] = abs(train['Horizontal_Distance_To_Fire_Points']+train['Horizontal_Distance_To_Roadways'])
-test['Fire_Road_1'] = abs(test['Horizontal_Distance_To_Fire_Points']+test['Horizontal_Distance_To_Roadways'])
+train['Fire_Road_1'] = abs(train['Horizontal_Distance_To_Fire_Points'] + train['Horizontal_Distance_To_Roadways'])
+test['Fire_Road_1'] = abs(test['Horizontal_Distance_To_Fire_Points'] + test['Horizontal_Distance_To_Roadways'])
 
-train['Fire_Road_2'] = abs(train['Horizontal_Distance_To_Fire_Points']-train['Horizontal_Distance_To_Roadways'])
-test['Fire_Road_2'] = abs(test['Horizontal_Distance_To_Fire_Points']-test['Horizontal_Distance_To_Roadways'])
+train['Fire_Road_2'] = abs(train['Horizontal_Distance_To_Fire_Points'] - train['Horizontal_Distance_To_Roadways'])
+test['Fire_Road_2'] = abs(test['Horizontal_Distance_To_Fire_Points'] - test['Horizontal_Distance_To_Roadways'])
 
 
 # Now let's make a submission using all those new features
 
-feature_cols = [col for col in train.columns if col not in ['Cover_Type','Id']]
+feature_cols = [col for col in train.columns if col not in ['Cover_Type', 'Id']]
 
 X_train = train[feature_cols]
 X_test = test[feature_cols]
@@ -103,20 +110,22 @@ test_ids = test['Id']
 #     bootstrap=False, oob_score=False, n_jobs=-1, random_state=None, verbose=0,
 #     min_density=None)
 
-forest = ensemble.RandomForestClassifier(n_estimators=500, n_jobs=-1)
-# forest = ensemble.ExtraTreesClassifier(n_estimators=400, n_jobs=-1)
+# forest = ensemble.RandomForestClassifier(n_estimators=500, n_jobs=-1)
+forest = ensemble.ExtraTreesClassifier(n_estimators=350, n_jobs=-1)
 
-print("Fitting...")
+print("%s" % time.time() + " : " + "Fitting...")
 forest.fit(X_train, y)
 
-# with open('features_engineering_benchmark.csv', "wb") as outfile:
-#     outfile.write("Id,Cover_Type\n")
-#     for e, val in enumerate(list(forest.predict(X_test))):
-#         outfile.write("%s,%s\n"%(test_ids[e],val))
+print("%s" % time.time() + " : " + "Predicting...")
+d = pd.DataFrame(0, index=np.arange(len(test['Id'])), columns=['Id', 'Cover_Type'])
+d['Id'] = test['Id']
+Y_test = forest.predict(X_test)
+d['Cover_Type'] = Y_test
+
+print("%s" % time.time() + " : " + "Writing to file...")
+d.to_csv('files/submission_' + datetime.now().strftime("%H%M%S") + ".csv", index=False)
 
 # look at feature importances
+# pd.DataFrame(forest.feature_importances_, index=X_train.columns).sort([0], ascending=False)[:10]
 
-pd.DataFrame(forest.feature_importances_,index=X_train.columns).sort([0], ascending=False) [:10]
-
-
-print("done")
+print("%s" % time.time() + " : " + "done")
